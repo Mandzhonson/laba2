fac4556e4c0e1b304c0efa5582920d08b8e0c7cb
manzhos.mihail <manzhos.mihail@mail.ru>
2024-11-26 23:22:06 +0300

    tweaked and reworked the code

--- applications.py
+++ applications.py
@@ -1,113 +1,73 @@
-import tkinter
 from tkinter import *
 from tkinter.messagebox import showinfo, askyesno
-
+from classes import Player
 
 class Application:
     master: Tk
-    canvas: Canvas
-    def draw_empty_field(self):
-        self.canvas.bind_all("<Button-1>",self.left_click)
-        step_x = (self.width//2) // 10
-        step_y = (self.height//2) // 10
-        step = min(step_x,step_y)
-        for i in range(11):
-            x = i * step
-            self.canvas.create_line(x+2*step, step*2, x+2*step, step*12, fill="blue")
-            self.canvas.create_line(x+20*step, step*2, x+20*step, step*12, fill="blue")
-        for i in range(11):
-            y = i * step
-            self.canvas.create_line(step*2, y+2*step,step*12, y+2*step,fill="blue")
-            self.canvas.create_line(step*20, y+2*step,x+20*step, y+2*step,fill="blue")
-        alph = "АБВГДЕЖЗИКЛ"
-        
-        for i in range(10):
-            self.canvas.create_text(step, step * 2 + i * step + step // 2, text=str(i + 1), anchor="w",font="Arial",fill="Blue")
-            self.canvas.create_text(19*step, step * 2 + i * step + step // 2, text=str(i + 1), anchor="w",font="Arial",fill="Blue")
-            self.canvas.create_text(step * 2 + i * step + step // 2, step*2-10, text=alph[i],font="Arial",fill="Blue")
-            self.canvas.create_text(step * 20 + i * step + step // 2, step*2-10, text=alph[i],font="Arial",fill="Blue")
-    def left_click(self,event):
-        pass
-    def click_exit(self) -> None:
-            # Операция закрытия приложения через меню
-        result = askyesno(title="Подтверждение выхода",
-                      message="Вы уверены, что хотите выйти?")
-        if result:
-            self.master.destroy()
-        else:
-            showinfo("Отмена операции", "Операция выхода отменена.")
-
-    def rules(self):
-        # окно с правилами игры
-        rul = Tk()
-        rul.title("Правила игры")
-        rule_text0 = "«Морской бой» — игра для двух участников, в которой игроки по очереди называют, сообщают иным способом, координаты на карте соперник. Если у врага с этими координатами имеется «корабль», то корабль или его палуба (дека) убивается, попавший делает еще один ход. Цель игрока: первым убить все «корабли» противника."
-        rule_text1 = "\n\nПРАВИЛА ИГРЫ:\nИгровое поле — обычно квадрат 10×10 у каждого игрока, на котором размещается флот кораблей. Горизонтали обычно нумеруются сверху вниз, а вертикали помечаются буквами слева направо. При этом используются буквы русского алфавита от «а» до «к» (буквы «ё» и «й» обычно пропускаются) либо от «а» до «и» (с использованием буквы «ё»), либо буквы латинского алфавита от «a» до «j». Иногда используется слово «республика» или «снегурочка», так как в этих 10-буквенных словах ни одна буква не повторяется. Поскольку существуют различные варианты задания системы координат, то об этом лучше заранее договориться."
-        rule_text2 = "\nРазмещаются:\n1 корабль — ряд из 4 клеток («четырёхпалубный»; линкор)\n2 корабля — ряд из 3 клеток («трёхпалубные»; крейсера)\n3 корабля — ряд из 2 клеток («двухпалубные»; эсминцы)\n4 корабля — 1 клетка («однопалубные»; торпедные катера)"
-        rule_text3 = "\nПри размещении корабли не могут касаться друг друга сторонами и углами. Встречаются, однако, варианты, когда касание углами не запрещается. Встречаются также варианты игры, когда корабли могут размещаться буквой «Г» («трёх-» и «четырёхпалубные»), квадратом или зигзагом («четырёхпалубные»). Кроме того, есть варианты с другим набором кораблей (напр., один пятипалубный, два четырёхпалубных и т. д.) и/или другой формой поля (15×15 для пятипалубных (авианосец))."
-        rule_text4 = "Рядом со «своим» полем чертится «чужое» такого же размера, только пустое. Это участок моря, где плавают корабли противника.\nПри попадании в корабль противника — на чужом поле ставится крестик, при холостом выстреле — точка. Попавший стреляет ещё раз."
-        rule_text = rule_text0+rule_text1+rule_text2+rule_text3+rule_text4
-        rul.geometry("900x400")
-        label2 = Label(master=rul, text=rule_text,
-                       wraplength=800, justify="left")
-        label2.grid(padx=10, pady=10)
-        btn = Button(master=rul, text="Ok", command=lambda: rul.destroy(
-        ), width=15, height=3, justify="center")
-        btn.grid()
-        rul.mainloop()
-
-    def create_menu(self):
-        # создание меню
-        main_menu = Menu(self.master)
-        new_game_menu = Menu(tearoff=0)
-        new_game_menu.add_command(label="1 player")
-        new_game_menu.add_command(label="2 players")
-        setting_menu = Menu(tearoff=0)
-        
-
-        screen_resol_menu = Menu(setting_menu, tearoff=0)
-        screen_resol_menu.add_command(
-            label="1280x720", command=lambda: self.create_main(1280,720))
-        screen_resol_menu.add_command(
-            label="1366x768", command=lambda: self.create_main(1366,768))
-        screen_resol_menu.add_command(
-            label="1440x900", command=lambda: self.create_main(1440,900))
-        screen_resol_menu.add_command(
-            label="1600x900", command=lambda: self.create_main(1600,900))
-        screen_resol_menu.add_command(
-            label="1920x1080", command=lambda: self.create_main(1920,1080))
-        screen_resol_menu.add_command(
-            label="1920x1200", command=lambda: self.create_main(1920,1200))
-        screen_resol_menu.add_command(
-            label="2560x1080", command=lambda: self.create_main(2560,1080))
-        screen_resol_menu.add_command(
-            label="2560x1440", command=lambda: self.create_main(2560,1440))
-
-        setting_menu.add_cascade(
-            label="Screen Resolution", menu=screen_resol_menu)
-        setting_menu.add_separator()
-        setting_menu.add_command(label="Rules", command=self.rules)
-        main_menu.add_cascade(label="New game", menu=new_game_menu)
-        main_menu.add_cascade(label="Settings", menu=setting_menu)
-        main_menu.add_cascade(label="Exit", command=self.click_exit)
-        self.master.config(menu=main_menu)
-
-    def create_main(self,width,height):
-        self.width = width
-        self.height = height
-        if self.canvas is not None:
-            self.canvas.destroy()
-        self.master.geometry(f"{self.width}x{self.height}+0+0")
-        self.canvas=Canvas(master=self.master,bg="white",width=self.width,height=self.height)
-        self.draw_empty_field()
-        self.canvas.pack()
-
-
-    def __init__(self, master) -> None:
+    def __init__(self,master):
         self.master = master
-        self.width = 1280
-        self.height = 720
-        self.canvas=None
-        self.create_main(self.width,self.height)
-        self.create_menu()
-        self.draw_empty_field()
+        self.pl1 = Player()
+        self.pl2 = Player(True)
+        self.current_player = self.pl1
+        self.field_pl1 = Canvas(bg="white",master=self.master,width=550,height=550)
+        self.field_pl2 = Canvas(bg="white",master=self.master,width=550,height=550)
+        self.field_pl1.pack(side= LEFT,padx=10,pady=10)
+        self.field_pl2.pack(side=RIGHT,padx=10,pady=10)
+        self.draw_field(self.field_pl1, self.pl1)
+        self.draw_field(self.field_pl2, self.pl2)
+        self.btn1=Button(text="1 игрок",command=lambda: self.start_game())
+        self.btn2=Button(text="2 игрока",command=lambda:self.start_game(True))
+        self.btn1.pack()
+        self.btn2.pack()
+    def draw_field(self,canvas:Tk,player:Player):
+        cell_size = 50
+        for row in range(10):
+            for col in range(10):
+                x1 = col * cell_size + cell_size
+                y1 = row * cell_size + cell_size
+                x2 = x1 + cell_size
+                y2 = y1 + cell_size
+
+                if player.get_ships()[row][col]:
+                    canvas.create_rectangle(x1, y1, x2, y2, fill="Gray", outline="Grey")
+                else:
+                    canvas.create_rectangle(x1, y1, x2, y2, outline="Blue")
+
+        alph = "АБВГДЕЖЗИК"
+        for i in range(10):
+            canvas.create_text((i + 1) * cell_size + cell_size // 2, cell_size // 2, text=alph[i])
+            canvas.create_text(cell_size // 2, (i + 1) * cell_size + cell_size // 2, text=str(i + 1))
+    def click_mouse(self,event):
+        y = event.x//50 - 1
+        x = event.y//50 - 1
+        if 0 <= x < 10 and 0 <= y < 10:
+            if self.current_player.get_count()<4:
+                self.current_player.place_ship(x,y,1)
+                self.draw_field(self.field_pl1, self.current_player)
+            elif self.current_player.get_count()>=4 and self.current_player.get_count()<7:
+                self.current_player.place_ship(x,y,2)
+                self.draw_field(self.field_pl1, self.current_player)
+            elif self.current_player.get_count()>=7 and self.current_player.get_count()<9:
+                self.current_player.place_ship(x,y,3)
+                self.draw_field(self.field_pl1, self.current_player)
+            elif self.current_player.get_count()==9:
+                self.current_player.place_ship(x,y,4)
+                self.draw_field(self.field_pl1, self.current_player)
+            else:   
+                print("10 plains on field")
+    def start_game(self,pl2=False):
+        showinfo("Подсказка","Разместите корабли на поле слева. Управление:\nЛКМ - примерное расположение корабля\nПКМ - подтверждение местоположения")
+        if not pl2:
+            count = 0
+            self.field_pl1.bind("<Button-1>",self.click_mouse)
+            while count!=20:
+                if count!=4:
+                    count+=1
+                elif count!=10:
+                    count+=2
+                elif count!=16:
+                    count+=3
+                else:
+                    count+=4
+        else:
+            pass
--- classes.py
+++ classes.py
@@ -1,1 +1,57 @@
+class Player:
+    __hits: list
+    __defeat_flag: bool
+    __ships: list
+    AI: bool
+    __count_plain: int
 
+    def __init__(self, AI=False) -> None:
+        self.__hits = [[False for _ in range(10)] for _ in range(10)]
+        self.__defeat_flag = False
+        self.AI = AI
+        self.__ships = [[False for _ in range(10)] for _ in range(10)]
+        self.__count_plain = 0
+
+    def get_hits(self) -> list:
+        return self.__hits
+
+    def get_ships(self) -> list[bool]:
+        return self.__ships
+
+    def get_flag(self) -> bool:
+        return self.__defeat_flag
+
+    def get_count(self) -> int:
+        return self.__count_plain
+
+    def place_ship(self, row, col, ship_length=1, orientation='horizontal'):
+        if orientation == 'horizontal':
+            if col + ship_length <= 10 and col + ship_length > 0:
+                for i in range(ship_length):
+                    self.__ships[row][col + i] = True
+                self.__count_plain += 1
+        elif orientation == 'vertical':
+            if row + ship_length <= 10 and row + ship_length > 0:
+                for i in range(ship_length):
+                    self.__ships[row + i][col] = True
+                self.__count_plain += 1
+
+    def can_place_ship(self, row, col, size, orientation):
+        if orientation == 'horizontal':
+            if col + size > 10:
+                return False
+            for i in range(size):
+                if self.ships[row][col + i]:
+                    return False
+                if col + i - 1 >= 0 and self.ships[row][col + i - 1]:
+                    return False
+                if col + i + 1 < 10 and self.ships[row][col + i + 1]:
+                    return False
+            if row - 1 >= 0:
+                for i in range(size):
+                    if self.ships[row - 1][col + i]:
+                        return False
+            if row + 1 < 10:
+                for i in range(size):
+                    if self.ships[row + 1][col + i]:
+                        return False
