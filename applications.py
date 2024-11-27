c65f92faf74d552f6a57ebb5efbea3f4295cedcb
manzhos.mihail <manzhos.mihail@mail.ru>
2024-11-27 14:17:03 +0300

    now it is possible to arrange ships for 2 players (there are bugs)

--- applications.py
+++ applications.py
@@ -1,25 +1,35 @@
 from tkinter import *
-from tkinter.messagebox import showinfo, askyesno
+from tkinter.messagebox import showinfo, showerror
 from classes import Player
+
 
 class Application:
     master: Tk
-    def __init__(self,master):
+
+    def __init__(self, master):
         self.master = master
         self.pl1 = Player()
         self.pl2 = Player(True)
+        self.fl_pl2 = False
         self.current_player = self.pl1
-        self.field_pl1 = Canvas(bg="white",master=self.master,width=550,height=550)
-        self.field_pl2 = Canvas(bg="white",master=self.master,width=550,height=550)
-        self.field_pl1.pack(side= LEFT,padx=10,pady=10)
-        self.field_pl2.pack(side=RIGHT,padx=10,pady=10)
+        self.field_pl1 = Canvas(
+            bg="white", master=self.master, width=550, height=550)
+        self.field_pl2 = Canvas(
+            bg="white", master=self.master, width=550, height=550)
+        self.field_pl1.pack(side=LEFT, padx=10, pady=10)
+        self.field_pl2.pack(side=RIGHT, padx=10, pady=10)
         self.draw_field(self.field_pl1, self.pl1)
         self.draw_field(self.field_pl2, self.pl2)
-        self.btn1=Button(text="1 игрок",command=lambda: self.start_game())
-        self.btn2=Button(text="2 игрока",command=lambda:self.start_game(True))
+        self.btn1 = Button(text="1 игрок", command=lambda: self.start_game())
+        self.btn2 = Button(text="2 игрока", command= lambda: (self.set_fl(True), self.start_game()))
+        self.orientation_var = StringVar(value='horizontal')
+        self.horizontal_radio = self.vertical_radio = None
+        self.btn_cancel = None
         self.btn1.pack()
         self.btn2.pack()
-    def draw_field(self,canvas:Tk,player:Player):
+    def set_fl(self,fl):
+        self.fl_pl2=fl
+    def draw_field(self, canvas: Tk, player: Player):
         cell_size = 50
         for row in range(10):
             for col in range(10):
@@ -27,47 +37,101 @@ class Application:
                 y1 = row * cell_size + cell_size
                 x2 = x1 + cell_size
                 y2 = y1 + cell_size
-
                 if player.get_ships()[row][col]:
-                    canvas.create_rectangle(x1, y1, x2, y2, fill="Gray", outline="Grey")
+                    canvas.create_rectangle(
+                        x1, y1, x2, y2, fill="Gray", outline="Grey")
                 else:
                     canvas.create_rectangle(x1, y1, x2, y2, outline="Blue")
 
         alph = "АБВГДЕЖЗИК"
         for i in range(10):
-            canvas.create_text((i + 1) * cell_size + cell_size // 2, cell_size // 2, text=alph[i])
-            canvas.create_text(cell_size // 2, (i + 1) * cell_size + cell_size // 2, text=str(i + 1))
-    def click_mouse(self,event):
-        y = event.x//50 - 1
-        x = event.y//50 - 1
+            canvas.create_text((i + 1) * cell_size + cell_size //
+                               2, cell_size // 2, text=alph[i])
+            canvas.create_text(cell_size // 2, (i + 1) *
+                               cell_size + cell_size // 2, text=str(i + 1))
+
+    def click_mouse(self, event):
+        canvas = self.field_pl1 if self.current_player == self.pl1 else self.field_pl2
+        y = event.x // 50 - 1
+        x = event.y // 50 - 1
+        size = None
+
         if 0 <= x < 10 and 0 <= y < 10:
-            if self.current_player.get_count()<4:
-                self.current_player.place_ship(x,y,1)
-                self.draw_field(self.field_pl1, self.current_player)
-            elif self.current_player.get_count()>=4 and self.current_player.get_count()<7:
-                self.current_player.place_ship(x,y,2)
-                self.draw_field(self.field_pl1, self.current_player)
-            elif self.current_player.get_count()>=7 and self.current_player.get_count()<9:
-                self.current_player.place_ship(x,y,3)
-                self.draw_field(self.field_pl1, self.current_player)
-            elif self.current_player.get_count()==9:
-                self.current_player.place_ship(x,y,4)
-                self.draw_field(self.field_pl1, self.current_player)
-            else:   
-                print("10 plains on field")
-    def start_game(self,pl2=False):
-        showinfo("Подсказка","Разместите корабли на поле слева. Управление:\nЛКМ - примерное расположение корабля\nПКМ - подтверждение местоположения")
-        if not pl2:
-            count = 0
-            self.field_pl1.bind("<Button-1>",self.click_mouse)
-            while count!=20:
-                if count!=4:
-                    count+=1
-                elif count!=10:
-                    count+=2
-                elif count!=16:
-                    count+=3
+            if self.current_player.get_count() < 4:
+                size = 1
+            elif self.current_player.get_count() < 7:
+                size = 2
+            elif self.current_player.get_count() < 9:
+                size = 3
+            elif self.current_player.get_count() == 9:
+                size = 4
+            else:
+                return
+
+            if size:
+                if self.horizontal_radio is None and self.vertical_radio is None:
+                    self.horizontal_radio = Radiobutton(
+                        self.master, text="Горизонтально", variable=self.orientation_var, value='horizontal')
+                    self.horizontal_radio.pack()
+                    self.vertical_radio = Radiobutton(
+                        self.master, text="Вертикально", variable=self.orientation_var, value='vertical')
+                    self.vertical_radio.pack()
+
+                orientation = self.orientation_var.get()
+                if self.current_player.can_place_ship(x, y, size, orientation):
+                    self.current_player.place_ship(x, y, size, orientation)
+                    self.draw_field(canvas, self.current_player)
                 else:
-                    count+=4
+                    showerror(title="Ошибка", message="Нельзя расположить корабли на данных клетках")
+
+                if self.btn_cancel is not None:
+                    self.btn_cancel.destroy()
+
+                self.btn_cancel = Button(
+                    master=self.master,
+                    text="Отмена действия",
+                    command=lambda: self.cancel_plain(x, y, size, orientation,canvas)
+                )
+                self.btn_cancel.pack()
+
+            # Переключение игроков при полной расстановке
+                if self.current_player.get_count() == 10:
+                    if self.fl_pl2 and self.current_player == self.pl1:
+                        self.btn_cancel.destroy()
+                        self.horizontal_radio.destroy()
+                        self.vertical_radio.destroy()
+                        self.horizontal_radio=self.vertical_radio=None
+                        self.switch_player()
+                        self.field_pl1.delete("all")  # Очищаем поле
+                    else:
+                        self.end_preparation()
+    def end_preparation(self):
+        showinfo("Игра началась", "Оба игрока разместили корабли. Начинаем сражение!")
+        self.field_pl1.unbind("<Button-1>")
+        self.field_pl2.unbind("<Button-1>")
+    def cancel_plain(self, x, y, size, orientation,canvas):
+        self.current_player.delete_ship(x, y, size, orientation)
+        canvas.delete("all")
+        self.draw_field(canvas, self.current_player)
+        if self.btn_cancel is not None:
+            self.btn_cancel.destroy()
+
+    def switch_player(self):
+        if self.current_player == self.pl1:
+            self.current_player = self.pl2
+            self.field_pl1.unbind("<Button-1>")  # Отключаем клики на поле игрока 1
+            self.field_pl2.bind("<Button-1>", self.click_mouse)  # Включаем клики на поле игрока 2
+            showinfo("Подсказка", "Теперь расставляет корабли игрок 2 на правом поле.")
         else:
-            pass
+            self.current_player = self.pl1
+            self.field_pl2.unbind("<Button-1>")
+            self.field_pl1.bind("<Button-1>", self.click_mouse)
+            showinfo("Игра началась", "Оба игрока разместили корабли. Начинаем сражение!")
+
+    def start_game(self):
+        showinfo("Подсказка", "Разместите корабли. Управление:\nЛКМ - постановка корабля\nКнопка \"Отмена действия\" - удаляет последний поставленный корабль")
+        if self.fl_pl2:
+            self.current_player = self.pl1
+            self.field_pl1.bind("<Button-1>", self.click_mouse)
+        else:
+            self.field_pl1.bind("<Button-1>", self.click_mouse)
--- classes.py
+++ classes.py
@@ -35,23 +35,39 @@ class Player:
                 for i in range(ship_length):
                     self.__ships[row + i][col] = True
                 self.__count_plain += 1
+    def delete_ship(self, row, col, ship_length, orientation='horizontal'):
+        if orientation=="horizontal":
+            for i in range(ship_length):
+                if col + ship_length <= 10 and col + ship_length > 0:
+                    self.__ships[row][col+i] = False
+            self.__count_plain-=1
 
+        elif orientation == 'vertical':
+            for i in range(ship_length):
+                if row + ship_length <= 10 and row + ship_length > 0:
+                    self.__ships[row+i][col] = False
+            self.__count_plain-=1
+    
     def can_place_ship(self, row, col, size, orientation):
         if orientation == 'horizontal':
             if col + size > 10:
                 return False
-            for i in range(size):
-                if self.ships[row][col + i]:
-                    return False
-                if col + i - 1 >= 0 and self.ships[row][col + i - 1]:
-                    return False
-                if col + i + 1 < 10 and self.ships[row][col + i + 1]:
-                    return False
-            if row - 1 >= 0:
-                for i in range(size):
-                    if self.ships[row - 1][col + i]:
-                        return False
-            if row + 1 < 10:
-                for i in range(size):
-                    if self.ships[row + 1][col + i]:
-                        return False
+        elif orientation == 'vertical':
+            if row + size > 10:
+                return False
+        for i in range(size):
+            if orientation == 'horizontal':
+                ship_row, ship_col = row, col + i
+            elif orientation == 'vertical':
+                ship_row, ship_col = row + i, col
+
+            if self.__ships[ship_row][ship_col]:
+                return False
+            for dr in [-1, 0, 1]:
+                for dc in [-1, 0, 1]:
+                    adj_row, adj_col = ship_row + dr, ship_col + dc
+                    if 0 <= adj_row < 10 and 0 <= adj_col < 10:
+                        if self.__ships[adj_row][adj_col]:
+                            return False
+        return True
+    
